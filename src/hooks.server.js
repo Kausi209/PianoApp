e37f4b9322cfda90e6c15b1ca74dbd1ce2538ac8
Kausi209 <uthaykau@students.zhaw.ch>
2026-01-09 21:03:24 +0100

    last fix

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,28 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
+import Google from "@auth/sveltekit/providers/google";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTH_SECRET } from "$env/static/private";
+import db from "$lib/db.js";
 
-export const handle = SvelteKitAuth({});
+export const { handle } = SvelteKitAuth({
+  providers: [
+    Google({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET
+    })
+  ],
+  secret: AUTH_SECRET,
+  trustHost: true,
+  callbacks: {
+    async session({ session }) {
+      const email = session?.user?.email;
+      if (email) {
+        await db.upsertUserFromGoogle({
+          email,
+          name: session.user?.name,
+          image: session.user?.image
+        });
+      }
+      return session;
+    }
+  }
+});
